de56178330aaf74ea530c61eb294caaee2ba37a7
Jason Boerner <jboerner56@gamil.com>
2019-04-04 14:38:04 -0400

    adds requirements to index.js file and adds beginnings of login-form

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const PORT = 3000;
 
 const freEbayRouter = require('./routes/freEbayRoutes');
 const app = express();
+const session = require('session');
+const FileStore = require('session-file-store')(session);
+app.use(session({
+    store: new FileStore(),
+    secret: 'asdfghjkl'
+}));
+const User = require('./models/users');
+const Item = require('./models/items');
+app.engine('html', es6Renderer);
+app.set('view engine', 'html');
 
+app.set('views', 'views'); 
+app.use(express.urlencoded({ extended: true }));
 app.use('/freEbay', freEbayRouter);
 
 
