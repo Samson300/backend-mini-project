a09e5c3159d19ad4d087269f4aa803e45ab330f5
Jason Boerner <jboerner56@gamil.com>
2019-04-04 15:09:21 -0400

    adds simple login page

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const PORT = 3000;
-
+const es6Renderer = require('express-es6-template-engine');
 const freEbayRouter = require('./routes/freEbayRoutes');
 const app = express();
-const session = require('session');
+const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 app.use(session({
     store: new FileStore(),
@@ -18,7 +18,9 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use('/freEbay', freEbayRouter);
 
-
+app.get('/login', (req, res) =>{
+    res.render('login-form')
+})
 app.listen(PORT, () => {
     console.log(`Running on Port: ${PORT}!`);
 })
