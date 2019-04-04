b218c657941529d246f7a2c36b64ab68dc27d062
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-04-04 10:54:25 -0400

    adds getAll and getById methods

--- model/users.js
+++ model/users.js
@@ -3,6 +3,7 @@ const db = require('./connect');
 const bcrypt = require('bcryptjs');
 
 class User {
+
     constructor(id, username, password, first_name, last_name, phone, email) {
         this.id = id;
         this.username = username;
@@ -12,7 +13,43 @@ class User {
         this.phone = phone;
         this.email = email;
     }
-        
+
+    static getAll() {
+        return db.any(`select * from users`)
+            .then((arrayOfUsers) => {
+                return arrayOfUsers.map((userData) => {
+                    const aUser = new User(
+                        userData.id,
+                        userData.username,
+                        userData.password,
+                        userData.first_name,
+                        userData.last_name,
+                        userData.phone,
+                        userData.email
+                        );
+                        return aUser;
+                    })
+                })
+            }
+            
+            static getById(id) {
+                return db.one(`select * from users where id=${id}`)
+                .then((userData) => {
+                    const userInstance = new User (
+                        userData.id,
+                        userData.username,
+                        userData.password,
+                        userData.first_name,
+                        userData.last_name,
+                        userData.phone,
+                        userData.email
+                );
+                return userInstance;
+            })
+            .catch(() => {
+                return null;
+            })
+    }
 }
 
 module.exports = User;
