52b7bb4dfb31bafb8ea205fe0e58b4ced6b735cf
Ryan Yim <yknyim@gmail.com>
2019-04-04 16:00:22 -0400

    reformats index.js in a readable fahsion

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const PORT = 3000;
-const es6Renderer = require('express-es6-template-engine');
-const freEbayRouter = require('./routes/freEbayRoutes');
 const app = express();
+
+// Session
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 app.use(session({
     store: new FileStore(),
     secret: 'asdfghjkl'
 }));
-const User = require('./models/users');
-const Item = require('./models/items');
+
+// Router
+const freEbayRouter = require('./routes/freEbayRoutes');
+app.use('/freEbay', freEbayRouter);
+
+// ES6 Templates
+const es6Renderer = require('express-es6-template-engine');
+app.set('views', './views'); 
 app.engine('html', es6Renderer);
 app.set('view engine', 'html');
 
-app.set('views', 'views'); 
-app.use(express.urlencoded({ extended: true }));
-app.use('/freEbay', freEbayRouter);
+// Models
+const User = require('./models/users');
+const Item = require('./models/items');
 
-app.get('/login', (req, res) =>{
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/freEbay', (req, res) => {
+    res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
     res.render('login-form')
 })
 
+// Listen on our Port
 app.listen(PORT, () => {
     console.log(`Running on Port: ${PORT}!`);
 })
