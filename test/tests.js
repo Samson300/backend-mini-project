53c1689bbef2f0c8bccc524536fff4b26e51220b
Ryan Yim <yknyim@gmail.com>
2019-04-04 13:33:49 -0400

    tests getAll and getById for items

--- test/tests.js
+++ test/tests.js
@@ -4,6 +4,7 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised).should();
 
 const User = require('../models/users');
+const Item = require('../models/items');
 
 describe('Users model', () => {
     it('should be able to retrieve all user data', async () => {
@@ -14,5 +15,16 @@ describe('Users model', () => {
     it('should be able to retrieve by id', async () => {
         const theUser = await User.getById(3);
         theUser.should.be.an.instanceOf(User);
+    });
+});
+
+describe('Items model', () => {
+    it('should be able to retrieve all item data', async () => {
+        const allItems = await Item.getAll();
+        allItems.should.be.an.instanceOf(Array);
+    });
+    it('should be able to retrieve by id', async () => {
+        const theItem = await Item.getById(2);
+        theItem.should.be.an.instanceOf(Item);
     });
 });
