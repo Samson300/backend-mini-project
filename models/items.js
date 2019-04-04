6441bbcb240cb900e93cdc6e25d7709b668a52c5
Jason Boerner <jboerner56@gamil.com>
2019-04-04 12:49:54 -0400

    adds getAll function to items

--- models/items.js
+++ models/items.js
@@ -1,5 +1,26 @@
 const db = require('./connect');
 
 class Item {
-    
+    constructor (id, name, description, price, picture){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.picture = picture;
+    }
+    static getAll() {
+        return db.any(`select * from items`)
+            .then((arrayofItems) => {
+                return arrayofItems.map((itemData) => {
+                    const anItem = new Item(
+                        itemData.id,
+                        itemData.name,
+                        itemData.description,
+                        itemData.price,
+                        itemData.picture
+                        )
+                    return anItem;
+                })
+            })
+    }
 }
